969844c812363482e93e3a8d00ae326d46d1b85c
Potzerus <Potzerus@gmail.com>
2019-04-30 21:51:40 +0200

    Implementing Save and Load feature

--- src/main/java/potz/utils/commands/AdminCommand.java
+++
@@ -1,8 +0,0 @@
-package potz.utils.commands;
-
-public abstract class AdminCommand extends Command {
-
-    public AdminCommand(String identifier) {
-        super(identifier);
-    }
-}
---
+++ src/main/java/potz/utils/commands/RestrictedCommand.java
@@ -0,0 +1,44 @@
+package potz.utils.commands;
+
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.permission.PermissionType;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import potz.Utils;
+
+public abstract class RestrictedCommand extends Command {
+
+    final protected PermissionType requiredPerm;
+
+
+    public RestrictedCommand(String identifier, PermissionType requiredPerm) {
+        super(identifier);
+        this.requiredPerm = requiredPerm;
+
+    }
+
+    public boolean hasPerm(User sender, TextChannel c, Server s) {
+        boolean output = Utils.hasPermission(sender, s, requiredPerm);
+        if (!output)
+            invalidPermResponse(c);
+        return output;
+    }
+
+    public boolean invalidPermResponse(TextChannel c) {
+        c.sendMessage("You have insufficient Permissions for this command you need " + requiredPerm);
+        return false;
+    }
+
+    public boolean notOwnerResponse(TextChannel c) {
+        c.sendMessage("Only the Bot owner can run this command");
+        return false;
+    }
+
+    public boolean isOwner(User sender,TextChannel c) {
+        if (sender.isBotOwner())
+            return true;
+        else
+            return notOwnerResponse(c);
+
+    }
+}
